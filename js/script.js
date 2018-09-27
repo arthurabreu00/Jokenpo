a3cade1a99a93216f8a8bff01a273e44e69fb321
Arthur Flávio de Abreu Tavares <277093@UTRAMIGBH.SEDE>
2018-09-26 22:26:58 -0300

    Resolvendo bugs.. terminar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.querySelector(".jogOne").addEventListener("click", () => {
 
     div = document.querySelectorAll(".hi2de");
-
+    var result = [0,0,0]
     for (let i = 0; i < 3; i++) {
         div[i].addEventListener("click",()=>{
-
-            console.log(div[i]);
-
+            var str = div[i]
+            result[i] = str.replace('<img src="imagens/papelPB.png" class="hi2de" alt=',"")
+            console.log(result[i]) 
         })
         
     }
